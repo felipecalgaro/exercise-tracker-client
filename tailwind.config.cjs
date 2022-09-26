962b8ee139a9b49610a14fed3f3d7570046cc15f
Felipe <pipecalgaro@gmail.com>
2022-09-26 19:33:31 -0300

    Revert "Set theme config"
    This reverts commit be334cf48fc3ee2966dab82dce1bedfdc49611f7.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,12 +7,11 @@ module.exports = {
         'black-custom': '#090909',
         'red-custom': '#ec4b4b',
         'light-yellow': '#ecb833',
-        'dark-yellow': '#635409',
+        'dark-yellow': '#cbad10',
         'light-green': '#17c73e',
         'dark-green': '#17680a',
         'purple-custom': '#390c5b',
-        'dark-orange': '#cb5a08',
-        'light-orange': '#cb9710',
+        'orange-custom': '#cb5a08',
       },
       fontFamily: {
         'roboto': 'Roboto'
