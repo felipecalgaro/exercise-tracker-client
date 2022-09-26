4b1d09e777713c5c9362afcf1cc269400a5a76cd
Felipe <pipecalgaro@gmail.com>
2022-09-26 19:36:20 -0300

    Update theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,11 +7,12 @@ module.exports = {
         'black-custom': '#090909',
         'red-custom': '#ec4b4b',
         'light-yellow': '#ecb833',
-        'dark-yellow': '#cbad10',
+        'dark-yellow': '#635409',
         'light-green': '#17c73e',
         'dark-green': '#17680a',
         'purple-custom': '#390c5b',
-        'orange-custom': '#cb5a08',
+        'dark-orange': '#cb5a08',
+        'light-orange': '#cb9710',
       },
       fontFamily: {
         'roboto': 'Roboto'
