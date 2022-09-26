be334cf48fc3ee2966dab82dce1bedfdc49611f7
Felipe <pipecalgaro@gmail.com>
2022-09-26 19:29:00 -0300

    Set theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black-custom': '#090909',
+        'red-custom': '#ec4b4b',
+        'light-yellow': '#ecb833',
+        'dark-yellow': '#cbad10',
+        'light-green': '#17c73e',
+        'dark-green': '#17680a',
+        'purple-custom': '#390c5b',
+        'orange-custom': '#cb5a08',
+      },
+      fontFamily: {
+        'roboto': 'Roboto'
+      }
+    },
   },
   plugins: [],
 }
