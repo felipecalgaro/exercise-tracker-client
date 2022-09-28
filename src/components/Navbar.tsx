f4152696e857d1790aae0e9dbc69e1ac9a77290d
Felipe <pipecalgaro@gmail.com>
2022-09-27 21:10:02 -0300

    Update Navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
 import { SuccessButton } from "./SuccessButton";
+import Logo from '../assets/Logo.svg'
+import { useState } from "react";
 
 export function Navbar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false)
+
   return (
-    <div className="bg-black-custom flex justify-between items-center w-full h-16 p-3">
-      <div className="bg-white h-8 w-8"></div>
-      <p className="font-roboto text-white">Logo</p>
+    <div className="bg-black-custom flex justify-between items-center w-full h-16 p-6">
+      <div className="flex justify-start items-center gap-2 p-2 flex-wrap w-16 hover:cursor-pointer" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+        <span className="bg-white h-1 w-10 rounded-sm"></span>
+        <span className={`bg-white h-1 ${isSidebarOpen ? 'w-5' : 'w-10'} transition-all duration-300 rounded-sm`}></span>
+        <span className="bg-white h-1 w-10 rounded-sm"></span>
+      </div>
+      <img src={Logo} alt='logo' className="h-10" />
       <SuccessButton text="New" />
     </div>
   )
